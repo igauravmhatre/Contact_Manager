36fef7a3edf95e7ceb93d492dd894c86877cb05c
Gaurav Mhatre <igauravmhatre>
2019-02-26 17:59:25 -0500

    Updates

---
+++ precache-manifest.47cfdc4a03da68f972129b2d6b29d73c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "c7c66851e895e2569df5",
+    "url": "/contactmanager/static/js/main.c7c66851.chunk.js"
+  },
+  {
+    "revision": "ae699ee8cd83b2a32632",
+    "url": "/contactmanager/static/js/1.ae699ee8.chunk.js"
+  },
+  {
+    "revision": "c7c66851e895e2569df5",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "ae699ee8cd83b2a32632",
+    "url": "/contactmanager/static/css/1.343957fb.chunk.css"
+  },
+  {
+    "revision": "3ea4e85069cc98981bd03f0a41bbfca1",
+    "url": "/contactmanager/index.html"
+  }
+];
